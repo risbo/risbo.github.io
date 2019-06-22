8a78c822ae87acacd79f6539203ffc1addea9390
Boris <bpalaciosa@gmail.com>
2019-06-21 23:11:19 -0500

    Test 01
    Test 01

---
+++ build/precache-manifest.3ca1b1aa5660f610c6514f3beca6573a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416992be275b852ca6eb302da298e7f2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4b5a00bcfca78c77d86",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "9b3a5007987034dff77a",
+    "url": "/static/js/2.652dea17.chunk.js"
+  },
+  {
+    "revision": "e4b5a00bcfca78c77d86",
+    "url": "/static/js/main.83be7ab5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
