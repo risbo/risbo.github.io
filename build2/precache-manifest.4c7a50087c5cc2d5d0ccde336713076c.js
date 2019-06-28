94fdd243fd1dab3df7e04bd3f5053cd78aefa01e
Boris <bpalaciosa@gmail.com>
2019-06-27 23:12:19 -0500

    Add files via upload

---
+++ build2/precache-manifest.4c7a50087c5cc2d5d0ccde336713076c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ddbb0af9006354a074f5cadd54ad5e12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6f996bc8caaef3034b7",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "f27c768a80e625806518",
+    "url": "/static/js/2.d9ba6776.chunk.js"
+  },
+  {
+    "revision": "e6f996bc8caaef3034b7",
+    "url": "/static/js/main.c6088979.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
